1174e0d54eba640fbb4dc40d9ae041216b7f9f0a
ykzhang721 <1114201025@qq.com>
2026-06-10 04:44:25 +0000

    EXP-004: add TrivialAugmentWide before ToTensor in train transform

--- train.py
+++ train.py
@@ -135,6 +135,7 @@ def main():
         [
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
+            transforms.TrivialAugmentWide(),
             transforms.ToTensor(),
             transforms.Normalize(mean, std),
             transforms.RandomErasing(
